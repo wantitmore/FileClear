dc6728631706bdad54c6a29e7cc29424bcb80718
xuyongke <894868351@qq.com>
2017-08-10 20:27:35 +0800

    引入清理缓存的AIDL文件

--- app/src/main/java/com/konka/fileclear/fragments/ClearMasterFragment.java
+++ app/src/main/java/com/konka/fileclear/fragments/ClearMasterFragment.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.konka.fileclear.R;
@@ -18,6 +20,9 @@ import com.konka.fileclear.utils.SdcardUtil;
 public class ClearMasterFragment extends Fragment {
 
 
+    private Button mOneKeyClear;
+    private TextView mLastClearTime, mLastClearSpace, mTotalClearSpace;
+
     public ClearMasterFragment() {
         // Required empty public constructor
     }
@@ -26,7 +31,27 @@ public class ClearMasterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_clear_master, container, false);
+        View view = inflater.inflate(R.layout.fragment_clear_master, container, false);
+        initView(view);
+        initListener();
+        return view;
+    }
+
+    private void initListener() {
+        mOneKeyClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // one key clear
+
+            }
+        });
+    }
+
+    private void initView(View view) {
+        mOneKeyClear = (Button) view.findViewById(R.id.btn_one_key_clear);
+        mLastClearTime = (TextView) view.findViewById(R.id.tv_last_clear_time);
+        mLastClearSpace = (TextView) view.findViewById(R.id.tv_last_clear_space);
+        mTotalClearSpace = (TextView) view.findViewById(R.id.tv_total_clear_space);
     }
 
     @Override
@@ -37,6 +62,7 @@ public class ClearMasterFragment extends Fragment {
         String sdAvailableSize = SdcardUtil.getSDAvailableSize(getActivity());
         Toast.makeText(getActivity(), sdTotalSize + "," + sdAvailableSize, Toast.LENGTH_SHORT).show();
 
+//        initListener();
     }
 
 }
