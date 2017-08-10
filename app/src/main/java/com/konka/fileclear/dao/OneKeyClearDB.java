450a6767c3f2cadb410c3b197f09d6a9d9bb984e
xuyongke <894868351@qq.com>
2017-08-10 16:31:15 +0800

    一键清理数据库建立

---
+++ app/src/main/java/com/konka/fileclear/dao/OneKeyClearDB.java
@@ -0,0 +1,8 @@
+package com.konka.fileclear.dao;
+
+/**
+ * Created by user001 on 2017-8-10.
+ */
+
+public class OneKeyClearDB {
+}
---
+++ app/src/main/java/com/konka/fileclear/dao/OneKeyClearTable.java
@@ -0,0 +1,9 @@
+package com.konka.fileclear.db;
+
+/**
+ * Created by user001 on 2017-8-10.
+ */
+
+public class OneKeyClearTable {
+    public static final String TABLE_NAME = ""
+}
