e70892eee32c1aba13a371367b144b5d74067edc
xuyongke <894868351@qq.com>
2017-08-26 21:32:54 +0800

    UI modify and appContorller finish

--- app/src/main/java/com/konka/fileclear/activity/MainActivity.java
+++ app/src/main/java/com/konka/fileclear/activity/MainActivity.java
@@ -8,12 +8,12 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.konka.fileclear.R;
 import com.konka.fileclear.fragments.AppControllerFragment;
@@ -24,17 +24,23 @@ import static com.konka.fileclear.R.id.rb_one_key_clear;
 
 public class MainActivity extends Activity implements View.OnFocusChangeListener{
 
-    private RadioGroup mClearGroup;
+    private LinearLayout mClearGroup;
     private AppControllerFragment mAppControllerFragment;
     private SpaceControllerFragment mSpaceControllerFragment;
     private ClearMasterFragment mOneKeyClearFragment;
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     public static final String TAG = "MainActivity";
-    private RadioButton mOneKeyClear, mSpaceController, mAppController;
+    private TextView mOneKeyClear, mSpaceController, mAppController;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
+    private ImageView lineAppManager;
+    private ImageView lineClearMaster;
+    private ImageView lineStorageManager;
+    private static final int CLEAR_MASTER = 0;
+    private static final int STORAGE_MANAGER = 1;
+    private static final int APP_MMANAGER = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,10 +95,13 @@ public class MainActivity extends Activity implements View.OnFocusChangeListener
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        mClearGroup = (RadioGroup) findViewById(R.id.rg_main);
-        mOneKeyClear = (RadioButton) findViewById(rb_one_key_clear);
-        mSpaceController = (RadioButton) findViewById(R.id.rb_space_controller);
-        mAppController = (RadioButton) findViewById(R.id.rb_app_controller);
+        mClearGroup = (LinearLayout) findViewById(R.id.rg_main);
+        mOneKeyClear = (TextView) findViewById(rb_one_key_clear);
+        mSpaceController = (TextView) findViewById(R.id.rb_space_controller);
+        mAppController = (TextView) findViewById(R.id.rb_app_controller);
+        lineAppManager = (ImageView) findViewById(R.id.line_app_manager);
+        lineClearMaster = (ImageView) findViewById(R.id.line_clear_master);
+        lineStorageManager = (ImageView) findViewById(R.id.line_storage_manager);
     }
 
 
@@ -103,7 +112,7 @@ public class MainActivity extends Activity implements View.OnFocusChangeListener
         Log.d(TAG, "onAttachedToWindow: " + measuredHeight);
     }
 
-    @Override
+/*    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (mOneKeyClearFragment.isVisible() && keyCode == KeyEvent.KEYCODE_DPAD_UP) {
             mClearGroup.check(R.id.rb_one_key_clear);
@@ -111,11 +120,10 @@ public class MainActivity extends Activity implements View.OnFocusChangeListener
             return false;
         }
         return super.onKeyDown(keyCode, event);
-    }
+    }*/
 
     @Override
     protected void onDestroy() {
-        Log.d(TAG, "onDestroy: onFocusChange:4");
         super.onDestroy();
     }
 
@@ -125,14 +133,34 @@ public class MainActivity extends Activity implements View.OnFocusChangeListener
             switch (v.getId()) {
                 case rb_one_key_clear :
                     switchFragment(mOneKeyClearFragment);
+                    setLineVisable(CLEAR_MASTER);
                     break;
                 case R.id.rb_space_controller :
                     switchFragment(mSpaceControllerFragment);
+                    setLineVisable(STORAGE_MANAGER);
                     break;
                 case R.id.rb_app_controller :
                     switchFragment(mAppControllerFragment);
+                    setLineVisable(APP_MMANAGER);
                     break;
             }
+        }
+    }
+
+    private void setLineVisable(int num) {
+        lineAppManager.setVisibility(View.INVISIBLE);
+        lineClearMaster.setVisibility(View.INVISIBLE);
+        lineStorageManager.setVisibility(View.INVISIBLE);
+        switch (num) {
+            case CLEAR_MASTER:
+                lineClearMaster.setVisibility(View.VISIBLE);
+                break;
+            case STORAGE_MANAGER:
+                lineStorageManager.setVisibility(View.VISIBLE);
+                break;
+            case APP_MMANAGER:
+                lineAppManager.setVisibility(View.VISIBLE);
+                break;
         }
     }
 }
--- app/src/main/java/com/konka/fileclear/fragments/AppControllerFragment.java
+++ app/src/main/java/com/konka/fileclear/fragments/AppControllerFragment.java
@@ -2,10 +2,13 @@ package com.konka.fileclear.fragments;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.konka.fileclear.R;
 
@@ -14,6 +17,8 @@ import com.konka.fileclear.R;
  */
 public class AppControllerFragment extends Fragment {
 
+
+    private Button mUninstallApp;
 
     public AppControllerFragment() {
         // Required empty public constructor
@@ -24,7 +29,27 @@ public class AppControllerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_controller, container, false);
+        View view = inflater.inflate(R.layout.fragment_app_controller, container, false);
+        mUninstallApp = (Button) view.findViewById(R.id.app_controller);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initListener();
+    }
+/*com.android.tv.settings/.device.apps.AppsActivity t244}*/
+    private void initListener() {
+        final Intent intent = new Intent();
+        String pkgName = "com.android.tv.settings";
+        String className = "com.android.tv.settings.device.apps.AppsActivity";
+        intent.setClassName(pkgName, className);
+        mUninstallApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
 }
